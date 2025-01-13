99b017439d5b09235ab120505b1c9c3b37baae22
Saqman77 <saqlainhaider77@gmail.com>
2025-01-13 18:20:48 +0500

    freedie and font sizes improved

--- Ash-p/src/pages/Freedie/Freedie.tsx
+++ Ash-p/src/pages/Freedie/Freedie.tsx
@@ -1,13 +1,25 @@
-
+import wheel from '../../assets/freedi/Brutalist 52.png'
+import './Freedie.scss'
 
 const Freedie = () => {
   return (
-    <div className="f-wrapper">
-      <div className="f-top">
-        <div className="f-left"></div>
-        <div className="f-right"></div>
+    <div className="f-container">
+      <div className="f-wrapper">
+        <div className="f-top">
+          <div className="f-left">
+              <h2 className="f-heading">
+                A supportive platform for all <br/><span className="f-colour">freelancers</span>
+              </h2>
+          </div>
+          <div className="f-right">
+              <div className='wheel'>
+                <img src={wheel} alt="wheel-image" className='wheel-img' />
+              </div>
+          </div>
+        </div>
+        <div className="f-bottom"></div>
       </div>
-      <div className="f-bottom"></div>
+      <div className="back-gear"></div>
     </div>
   )
 }
