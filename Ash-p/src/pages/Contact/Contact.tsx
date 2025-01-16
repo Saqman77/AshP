5d59f35c1e8e2bfe390c5fba2c23c4329386090f
Saqman77 <saqlainhaider77@gmail.com>
2025-01-16 15:47:34 +0500

    setup

--- Ash-p/src/pages/Contact/Contact.tsx
+++ Ash-p/src/pages/Contact/Contact.tsx
@@ -1,8 +1,33 @@
-
+import './Contact.scss'
+import FB from '../../assets/freedi/Facebook.png'
+import INSTA from '../../assets/freedi/insta.png'
+import X from '../../assets/freedi/X.png'
+import LINK from '../../assets/freedi/linked.png'
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <div className="c-wrapper">
+      <div className="c-top">
+        <div className="c-left"></div>
+        <div className="c-right"></div>
+      </div>
+      <div className="c-bottom">
+        <ul className="c-info">
+          <li></li>
+          <li></li>
+        </ul>
+        <ul className="c-location">
+          <li></li>
+          <li></li>
+        </ul>
+        <ul className="c-socials">
+          <li><a href=""><img src={FB} alt="" className='social' /></a></li>
+          <li><a href=""><img src={INSTA} alt="" className='social' /></a></li>
+          <li><a href=""><img src={X} alt="" className='social' /></a></li>
+          <li><a href=""><img src={LINK} alt="" className='social' /></a></li>
+        </ul>
+      </div>
+    </div>
   )
 }
 
