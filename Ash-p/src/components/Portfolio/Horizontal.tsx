7ab166f4d5e08c87c5001ea68b9ae0d81010276b
Saqman77 <saqlainhaider77@gmail.com>
2025-01-18 23:02:58 +0500

    tsx setup

--- Ash-p/src/components/Portfolio/Horizontal.tsx
+++ Ash-p/src/components/Portfolio/Horizontal.tsx
@@ -1,8 +1,28 @@
 import './horizontal.scss'
-
+import React, { useEffect, useRef } from "react";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 const Horizontal = () => {
   return (
-    <div>Horizontal</div>
+    <div className="h-container">
+        <section className='h-wrapper'>
+            <h1>Who we are?</h1>
+
+            <div className="h-card" id='h-card1'>
+                <img src="../Frame 21.svg" alt="" />
+            </div>
+            <div className="h-card" id='h-card2'>
+                <img src="../Frame 21.svg" alt="" />
+            </div>
+            <div className="h-card" id='h-card3'>
+                <img src="../Frame 21.svg" alt="" />
+            </div>
+            <div className="h-card" id='h-card4'>
+                <img src="../Frame 21.svg" alt="" />
+            </div>
+        </section>
+        <section className='outro'></section>
+    </div>
   )
 }
 
