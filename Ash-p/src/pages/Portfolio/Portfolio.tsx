1f96444a0633b441b985435f5565d410ce001188
Saqman77 <saqlainhaider77@gmail.com>
2025-01-19 08:45:17 +0500

    Update Portfolio.tsx

--- Ash-p/src/pages/Portfolio/Portfolio.tsx
+++ Ash-p/src/pages/Portfolio/Portfolio.tsx
@@ -5,6 +5,21 @@ import './Portfolio.scss'
 const Portfolio = () => {
   return (
     <div className="a-container">
+      <h1
+        style={{
+          position:'fixed',
+          color: 'red',
+          top: '50%',
+          left: '50%',
+          transform: 'translate(-50%, -50%)',
+          zIndex: '99999',
+          boxShadow: 'inset 0px 0px 4px 8px rgba(255, 0, 0, 0.17)',
+          fontSize: '5vw'
+        }}
+
+      >
+        Under construction
+      </h1>
       <Horizontal/>
       <Scroll/>
     </div>
