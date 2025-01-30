6e4c2c4daabe2b3c89f6f1cf45407cb32bbd69ed
Saqman77 <saqlainhaider77@gmail.com>
2025-01-30 09:23:01 +0500

    object structure

---
+++ Ash-p/src/components/Portfolio/services.ts
@@ -0,0 +1,34 @@
+export const services = [
+    {
+        service:'Full Editing Services',
+        id:'Full Editing Services',
+        clients:[
+            {           
+                name:'TC Publishing',
+                type:'Nonfiction',
+                projects:[
+                    {
+                        project:'Overcoming Hardship – Senada Dervisevic',
+                        link:'https://a.co/d/0Aq7xUn',
+                    },
+                    {
+                        project:'Miracles and Magic: A story of faith, resilience and overcoming adversity – Noreen Clarke',
+                        link:'https://a.co/d/7WBdTTt',
+                    },
+                    {
+                        project:'Tha Game Changa: Fuel your body, empower your mind- through food, fitness, and motivation. – Horace Jones',
+                        link:'https://amzn.eu/d/18qHbq3',
+                    },
+                    {
+                        project:'Seasons of Change – Trish Nelson',
+                        link:'',
+                    },
+                    {
+                        project:'THE REAPing WOMAN: A Step-by-Step Guide to a Purposeful Life – Dr. Catherine Muyeba ',
+                        link:'',
+                    },
+                ]
+            }
+        ]
+    }
+]
