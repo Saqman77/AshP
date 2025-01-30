4d1c8389fb1b16501edce1977597f76525bf0d68
Saqman77 <saqlainhaider77@gmail.com>
2025-01-30 13:24:53 +0500

    services content

--- Ash-p/src/components/Portfolio/services.ts
+++ Ash-p/src/components/Portfolio/services.ts
@@ -5,30 +5,400 @@ export const services = [
         clients:[
             {           
                 name:'TC Publishing',
-                type:'Nonfiction',
-                projects:[
+                types:[
                     {
-                        project:'Overcoming Hardship – Senada Dervisevic',
-                        link:'https://a.co/d/0Aq7xUn',
+                    type:'Nonfiction',
+                    projects:[
+                        {
+                            project:'Overcoming Hardship – Senada Dervisevic',
+                            link:'https://a.co/d/0Aq7xUn',
+                        },
+                        {
+                            project:'Miracles and Magic: A story of faith, resilience and overcoming adversity – Noreen Clarke',
+                            link:'https://a.co/d/7WBdTTt',
+                        },
+                        {
+                            project:'Tha Game Changa: Fuel your body, empower your mind- through food, fitness, and motivation. – Horace Jones',
+                            link:'https://amzn.eu/d/18qHbq3',
+                        },
+                        {
+                            project:'Seasons of Change – Trish Nelson',
+                            link:'',
+                        },
+                        {
+                            project:'THE REAPing WOMAN: A Step-by-Step Guide to a Purposeful Life – Dr. Catherine Muyeba ',
+                            link:'',
+                        },
+                    ]
                     },
                     {
-                        project:'Miracles and Magic: A story of faith, resilience and overcoming adversity – Noreen Clarke',
-                        link:'https://a.co/d/7WBdTTt',
-                    },
-                    {
-                        project:'Tha Game Changa: Fuel your body, empower your mind- through food, fitness, and motivation. – Horace Jones',
-                        link:'https://amzn.eu/d/18qHbq3',
-                    },
-                    {
-                        project:'Seasons of Change – Trish Nelson',
-                        link:'',
-                    },
-                    {
-                        project:'THE REAPing WOMAN: A Step-by-Step Guide to a Purposeful Life – Dr. Catherine Muyeba ',
-                        link:'',
+                        type:'Fiction',
+                        projects:[{}]
                     },
                 ]
             }
         ]
-    }
+    },
+    {
+        service:'Developmental Editing',
+        id:'Developmental Editing',
+        clients:[
+            {           
+                name:'Shena Lashey',
+                types:[
+                    {
+                    type:'Nonfiction',
+                    projects:[
+                        {
+                            project:'How to Pick Partners for Love Addicts: A Comprehensive Relationship Guide to Breaking Free from Toxic Patterns and Choosing Those Who Are Ready for Love',
+                            link:'https://www.amazon.com/Pick-Partners-Love-Addicts-Comprehensive',
+                        },
+                        {
+                            project:'No Contact & Detox Support book',
+                            link:'',
+                        },
+                        
+                    ]
+                    },
+                    {
+                        type:'Fiction',
+                        projects:[{}]
+                    },
+                ]
+            }
+        ]
+    },
+    {
+        service:'Proofreading',
+        id:'Proofreading',
+        clients:[
+            {           
+                name:'TC Publishing',
+                types:[
+                    {
+                    type:'Nonfiction',
+                    projects:[
+                        {
+                            project:'The simple way to get motivated –Tarnya Coley',
+                            link:'https://a.co/d/c4daISa',
+                        },
+                        {
+                            project:'This Is My Story: A True Story Abuse, Forgiveness & God’s Love – Mary Joy',
+                            link:'https://a.co/d/9ulwK7m',
+                        },
+                        {
+                            project:'Speaking Pictures: A collection of poems to encourage a looking beyond those things which are seen – Alfred Gerrity',
+                            link:'https://a.co/d/81wDpN8',
+                        },
+                        {
+                            project:'Persistent Poems: A compilation of prophetic poems to awaken a deceived generation – Vicky Ash',
+                            link:'https://a.co/d/89lcaUt',
+                        },
+                        {
+                            project:'Ignite Your Author – Tarnya Coley',
+                            link:'',
+                        },
+                    ]
+                    },
+                    {
+                        type:'Fiction',
+                        projects:[{}]
+                    },
+                ]
+            },
+            {           
+                name:'Shena Lashey',
+                types:[
+                    {
+                    type:'Nonfiction',
+                    projects:[
+                        {
+                            project:'How to Pick Partners for Love Addicts: A Comprehensive Relationship Guide to Breaking Free from Toxic Patterns and Choosing Those Who Are Ready for Love',
+                            link:'https://www.amazon.com/Pick-Partners-Love-Addicts-Comprehensive',
+                        }                       
+                    ]
+                    },
+                    {
+                        type:'Fiction',
+                        projects:[{}]
+                    },
+                ]
+            },
+            {           
+                name:'Grea Alexander',
+                types:[
+                    {
+                    type:'Nonfiction',
+                    projects:[
+                        {
+                            project:'',
+                            link:'',
+                        }                       
+                    ]
+                    },
+                    {
+                        type:'Fiction',
+                        projects:[
+                            {
+                            project:'All series by author',
+                            link:'https://greaalexander.seamonkeyink.com/grea_alexander_009.html',
+                        },
+
+                    ]
+                    },
+                ]
+            },
+            {           
+                name:'Charlene Hartnady',
+                types:[
+                    {
+                    type:'Nonfiction',
+                    projects:[
+                        {
+                            project:'',
+                            link:'',
+                        }                       
+                    ]
+                    },
+                    {
+                        type:'Fiction',
+                        projects:[
+                            {
+                            project:'All series by author',
+                            link:'https://charlenehartnady.com/charlene-hartnady--books.html',
+                        },
+                    ]
+                    },
+                ]
+            },
+            {           
+                name:'B.E. Brouillard',
+                types:[
+                    {
+                    type:'Nonfiction',
+                    projects:[
+                        {
+                            project:'',
+                            link:'',
+                        }                       
+                    ]
+                    },
+                    {
+                        type:'Fiction',
+                        projects:[
+                            {
+                            project:'Exiled by the Pack (with Charlene Hartnady) 4 Books',
+                            link:'https://www.amazon.com/stores/author/B089K8WPHY',
+                        },
+                            {
+                            project:'Becoming the Demon Queen (with Charlene Hartnady) 3 Books ',
+                            link:'https://www.amazon.com/stores/author/B089K8WPHY',
+                        },
+                            {
+                            project:'Becoming the Fae Queen (with Charlene Hartnady) 2 Books',
+                            link:'https://www.amazon.com/stores/author/B089K8WPHY',
+                        },
+                            {
+                            project:'Becoming the Vampire Queen 2 Books',
+                            link:'https://www.amazon.com/stores/author/B089K8WPHY',
+                        },
+                            {
+                            project:'Becoming the Orc Queen 2 Books',
+                            link:'https://www.amazon.com/stores/author/B089K8WPHY',
+                        },
+                    ]
+                    },
+                ]
+            },
+        ]
+    },
+    {
+        service:'Beta Reading',
+        id:'Beta Reading',
+        clients:[
+            {           
+                name:'Ellis Prybylsk',
+                types:[
+                    {
+                    type:'Nonfiction',
+                    projects:[
+                        {
+                            project:'How To Publish The Damn Book',
+                            link:'https://a.co/d/jdObCai',
+                        },
+                    ]
+                    },
+                    {
+                        type:'Fiction',
+                        projects:[{}]
+                    },
+                ]
+            },
+            {           
+                name:'Gillian Grant',
+                types:[
+                    {
+                    type:'Nonfiction',
+                    projects:[
+                        {},
+                    ]
+                    },
+                    {
+                        type:'Fiction',
+                        projects:[
+                            {
+                            project:'Where the Heart Festers – Book 3 in the Blood of Eith series',
+                            link:'https://a.co/d/799cvYy',
+                        }
+                    ]
+                    },
+                ]
+            },
+            {           
+                name:'Elisabeth Naughton',
+                types:[
+                    {
+                    type:'Nonfiction',
+                    projects:[
+                        {
+                            
+                        },
+                    ]
+                    },
+                    {
+                        type:'Fiction',
+                        projects:[
+                            {
+                            project:'Books 1-3 of the House of Sin series',
+                            link:'https://www.amazon.com/dp/B0BCCK33P2?binding=kindle_edition&ref=dbs_dp_rwt_sb_pc_tkin',
+                            }]
+                    },
+                ]
+            },
+            {           
+                name:'Charlene Hartnady',
+                types:[
+                    {
+                    type:'Nonfiction',
+                    projects:[
+                        {
+                            project:'How To Publish The Damn Book',
+                            link:'https://a.co/d/jdObCai',
+                        },
+                    ]
+                    },
+                    {
+                        type:'Fiction',
+                        projects:[
+                            {
+                                project:'All series by the author',
+                                link:'https://charlenehartnady.com/charlene-hartnady--books.html',
+                            }]
+                    },
+                ]
+            },
+            {           
+                name:'Grea Alexander',
+                types:[
+                    {
+                    type:'Nonfiction',
+                    projects:[
+                        {
+
+                        },
+                    ]
+                    },
+                    {
+                        type:'Fiction',
+                        projects:[
+                            {
+                                project:'All series by author',
+                                link:'https://greaalexander.seamonkeyink.com/grea_alexander_009.html',
+                            },
+                           
+                        ]
+                    },
+                ]
+            },
+            {           
+                name:'B.E. Brouillard',
+                types:[
+                    {
+                    type:'Nonfiction',
+                    projects:[
+                        {
+
+                        },
+                    ]
+                    },
+                    {
+                        type:'Fiction',
+                        projects:[
+                            {
+                                project:'Exiled by the Pack (with Charlene Hartnady) 4 Books',
+                                link:'https://www.amazon.com/stores/author/B089K8WPHY',
+                            },
+                            {
+                                project:'Becoming the Demon Queen (with Charlene Hartnady) 3 Books ',
+                                link:'https://www.amazon.com/stores/author/B089K8WPHY',
+                            },
+                            {
+                                project:'Becoming the Fae Queen (with Charlene Hartnady) 2 Books',
+                                link:'https://www.amazon.com/stores/author/B089K8WPHY',
+                            },
+                            {
+                                project:'Becoming the Vampire Queen 2 Books',
+                                link:'https://www.amazon.com/stores/author/B089K8WPHY',
+                            },
+                            {
+                                project:'Becoming the Orc Queen 2 Books',
+                                link:'https://www.amazon.com/stores/author/B089K8WPHY',
+                            },
+                        ]
+                    },
+                ]
+            },
+        ]
+    },
+    {
+        service:'Full Editing Services',
+        id:'Full Editing Services',
+        clients:[
+            {           
+                name:'TC Publishing',
+                types:[
+                    {
+                    type:'Nonfiction',
+                    projects:[
+                        {
+                            project:'Overcoming Hardship – Senada Dervisevic',
+                            link:'https://a.co/d/0Aq7xUn',
+                        },
+                        {
+                            project:'Miracles and Magic: A story of faith, resilience and overcoming adversity – Noreen Clarke',
+                            link:'https://a.co/d/7WBdTTt',
+                        },
+                        {
+                            project:'Tha Game Changa: Fuel your body, empower your mind- through food, fitness, and motivation. – Horace Jones',
+                            link:'https://amzn.eu/d/18qHbq3',
+                        },
+                        {
+                            project:'Seasons of Change – Trish Nelson',
+                            link:'',
+                        },
+                        {
+                            project:'THE REAPing WOMAN: A Step-by-Step Guide to a Purposeful Life – Dr. Catherine Muyeba ',
+                            link:'',
+                        },
+                    ]
+                    },
+                    {
+                        type:'Fiction',
+                        projects:[{}]
+                    },
+                ]
+            }
+        ]
+    },
+    
 ]
