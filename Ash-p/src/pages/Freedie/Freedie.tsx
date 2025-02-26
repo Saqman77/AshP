345e06f9730f2fc5efecc54d997061b4bf171897
Saqman77 <saqlainhaider77@gmail.com>
2025-02-26 09:04:46 +0500

    huge changes

--- Ash-p/src/pages/Freedie/Freedie.tsx
+++ Ash-p/src/pages/Freedie/Freedie.tsx
@@ -1,17 +1,23 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 import wheel from '../../assets/freedi/Brutalist 52.png';
 import gear from '../../assets/freedi/Vector2.svg';
 import FLists from '../../components/freedie/flist/FLists';
+import frediebg from '../../assets/freedi/FrEdiBuddies without title.webp'
 import './Freedie.scss';
 import FCarousel from '../../components/freedie/fcarousel/FCarousel';
 import { useThemeContext } from '../../utils/ThemeContextProvider';
+import ParallaxImage from '../../components/Portfolio/ParallaxImage';
+import { useGSAP } from '@gsap/react';
+gsap.registerPlugin(ScrollTrigger);
 
 const Freedie: React.FC = () => {
   const [selectedIndex, setSelectedIndex] = useState(0);
   const [isCarouselVisible, setCarouselVisible] = useState(false);
   const [scrollY, setScrollY] = useState(0);
   const [showDisclaimer, setShowDisclaimer] = useState(true); // Show disclaimer initially
-
+  const bgref = useRef<HTMLDivElement>(null)
   const { isActive, removeClass } = useThemeContext();
 
   useEffect(() => {
@@ -44,6 +50,79 @@ const Freedie: React.FC = () => {
   const handleCloseCarousel = () => {
     setCarouselVisible(false);
   };
+  useGSAP(() => {
+    const tl = gsap.timeline();
+    gsap.fromTo('.fredie-bg', 
+      {
+         clipPath: 'polygon(0 0, 0 0, 0 0, 0 0)',
+         opacity:0.5, 
+      }, // Fully hidden
+      { 
+        clipPath: 'polygon(0 0, 100% 0, 100% 100%, 0% 100%)', // Full rectangle
+        ease: 'none',
+        opacity:1,
+        scrollTrigger: {
+          trigger: '.fredie-bg',
+          start: 'top 70%', // Starts when .fredie-bg enters the viewport
+          end: 'top 20%', // Completes near the top
+          scrub: 1, // Smoothly linked to scrolling
+        }
+      });
+
+      tl.to('.fredie-content',{
+        background:'linear-gradient(to bottom, rgba(0, 0, 0, 0.8) 0%, rgba(0, 0, 0, 0) 50%, rgba(0, 0, 0, 0.8) 100%)',
+        backdropFilter:'blur(5px)',
+        webkitBackdropFilter:'blur(5px)',
+        scrollTrigger: {
+          trigger: '.fredie-bg',
+          start: 'top 30%', // Starts when .fredie-bg enters the viewport
+          end: 'top 20%', // Completes near the top
+          scrub: 1, // Smoothly linked to scrolling
+        }
+      })
+      tl.to('.fredie-text span',
+      {
+        // backgroundColor:'#33333365',
+        // backdropFilter:'blur(5px)',
+        // webkitBackdropFilter:'blur(5px)',
+        color:'#f9f9f9',
+        scrollTrigger: {
+          trigger: '.fredie-bg',
+          start: 'top 30%', // Starts when .fredie-bg enters the viewport
+          end: 'top 20%', // Completes near the top
+          scrub: 1, // Smoothly linked to scrolling
+        }
+      })
+      tl.fromTo('.fredie-text',{
+        transform:'translateY(200%)'
+      },{
+        // backgroundColor:'#33333365',
+        // backdropFilter:'blur(5px)',
+        // webkitBackdropFilter:'blur(5px)',
+        transform:'translateY(0%)',
+        // color:'#f9f9f9',
+        scrollTrigger: {
+          trigger: '.fredie-bg',
+          start: 'top 70%', // Starts when .fredie-bg enters the viewport
+          end: 'center 20%', // Completes near the top
+          scrub: 1, // Smoothly linked to scrolling
+        }
+      })
+      tl.to('.fredie-section',{
+        // backgroundColor:'#33333365',
+        // backdropFilter:'blur(5px)',
+        // webkitBackdropFilter:'blur(5px)',
+        boxShadow:'0px 0px 4px 3px rgba(0, 0, 0, 0.514)',
+        // color:'#f9f9f9',
+        scrollTrigger: {
+          trigger: '.fredie-bg',
+          start: 'top 30%', // Starts when .fredie-bg enters the viewport
+          end: 'top 20%', // Completes near the top
+          scrub: 1, // Smoothly linked to scrolling
+        }
+      })
+  }, { scope: bgref });
+  
 
   return (
     <>
@@ -74,7 +153,9 @@ const Freedie: React.FC = () => {
         onClose={handleCloseCarousel}
       />
 
-      <div className="f-container">
+      <div className="f-container"
+        ref={bgref}
+      >
         <div className="f-wrapper">
           <div className="f-top">
             <div className="f-left">
@@ -100,14 +181,29 @@ const Freedie: React.FC = () => {
             </div>
           </div>
           <section className='fredie-section'>
-            <div className='fredi-desc'>
-            <span>
-                When Ash P started out as an independent service provider, 20 or so years ago, the market was fairly new in Pakistan. The learning curve was steep and much was learned through trial and error. Now that independent services and working remotely is recognized as a “real” job, it is an option that many prefer to a regular 9-5 career.
-                </span>
-                <span className="spacer"></span>
+            
+            <div className='fredie-content'>
+              <p className='fredie-text'>
                 <span>
-                Keeping that in mind, Ash P Reads FrEdiBuddies was created to give budding independent service providers a platform to hone their skills and a safe space where projects and clients are vetted, to prevent them from falling victim to fakes and scam artists.  
+                    When Ash P started out as an independent service provider, 20 or so years ago, the market was fairly new in Pakistan. The learning curve was steep and much was learned through trial and error. Now that independent services and working remotely is recognized as a “real” job, it is an option that many prefer to a regular 9-5 career.
+                </span>                  
+              </p>
+              <span className='seperator'></span>
+              <p className='fredie-text'>
+                <span>
+                    Keeping that in mind, Ash P Reads FrEdiBuddies was created to give budding independent service providers a platform to hone their skills and a safe space where projects and clients are vetted, to prevent them from falling victim to fakes and scam artists.
                 </span>
+              </p>
+            </div>
+            <div className="fredie-bg"
+
+            >
+              <div className="fredie-img">
+                <ParallaxImage
+                  src={frediebg}
+                  alt='background'
+                />
+              </div>
             </div>
           </section>
           <div className="f-bottom">
