1e4beace2b244b6b926404f0060ae2d0669d0bc5
Saqman77 <saqlainhaider77@gmail.com>
2025-02-27 07:56:58 +0500

    Update freedie-para.tsx

--- Ash-p/src/components/freedie/freedie-para.tsx
+++ Ash-p/src/components/freedie/freedie-para.tsx
@@ -39,7 +39,7 @@ const FreediePara: React.FC<ParallaxImageProps> = ({ src, alt, className }) => {
           0.1
         );
         if (Math.abs(currentTranslateY.current - targetTranslateY.current) > 0.01) {
-          imageRef.current.style.transform = `translateY(${currentTranslateY.current}px) scale(1.25)`;
+          imageRef.current.style.transform = `translateY(${currentTranslateY.current}px) scale(1)`;
         }
       }
       rafId.current = requestAnimationFrame(animate);
