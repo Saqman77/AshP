97f11f51b8be2d50fe4a201141ee9d60301f84b8
Saqlain Haider <saqlainhaider77@gmail.com>
2025-06-05 12:55:13 +0500

    home page

--- Ash-p/src/pages/Home/Home.tsx
+++ Ash-p/src/pages/Home/Home.tsx
@@ -1,12 +1,14 @@
 import './Home.scss';
 import Hero from '../../components/Home/Hero/Hero';
 import Results from '../../components/Home/Results/Results';
+import ThirdSection from '../../components/Home/ThirdSection/ThirdSection';
 
 const Home: React.FC = () => {
   return (
     <div className="home">
       <Hero />
       <Results />
+      <ThirdSection />
     </div>
   );
 };
