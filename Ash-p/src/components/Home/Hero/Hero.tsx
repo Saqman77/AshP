3e8e49d8007c3d8ac62f53c50e1d5380b94cdbf1
Saqlain Haider <saqlainhaider77@gmail.com>
2025-06-05 07:03:55 +0500

    hero content almost done desktop

--- Ash-p/src/components/Home/Hero/Hero.tsx
+++ Ash-p/src/components/Home/Hero/Hero.tsx
@@ -1,5 +1,8 @@
+import ContactUs from '../../get-in-touch-button/ContactUs';
+import Schedule from '../../schedule/Schedule';
 import './Hero.scss';
 import { heroContent } from './heroContent';
+import downArrow from '/src/assets/home/arrow-down.svg';
 
 const Hero: React.FC = () => {
   const highlightWords = (text: string) => {
@@ -22,6 +25,16 @@ const Hero: React.FC = () => {
       <div className="hero-main">
         <div className="content-wrapper">
           <h1>{highlightWords(heroContent.heading)}</h1>
+          <div className="cta-wrapper">
+            <ContactUs/>
+            <Schedule />
+          </div>
+          <div className="hero-box">
+            <p className="scroll-down">{heroContent.scrollDown}</p>
+            <div className="arrow-wrapper">
+              <img src={downArrow} alt="down-arrow" className="down-arrow" />
+            </div>
+          </div>
         </div>
       </div>
     </div>
--- Ash-p/src/components/schedule/Schedule.tsx
+++ Ash-p/src/components/schedule/Schedule.tsx
@@ -1,8 +1,14 @@
 import './schedule.scss'
+import arrow from '/src/assets/buttons/right-arrow.svg';
 
 const Schedule = () => {
   return (
-    <a href="https://calendly.com/ashpreads" className='schedule-button'><button>Book a free video consultation</button></a>
+    <a href="https://calendly.com/ashpreads" className='schedule-button'><button>        <div className="contact-us-text">
+    <p>Book a free video consultation</p>
+  </div>
+  <div className="contact-us-arrow">
+    <img src={arrow} alt="right-arrow" />
+  </div></button></a>
   )
 }
 
