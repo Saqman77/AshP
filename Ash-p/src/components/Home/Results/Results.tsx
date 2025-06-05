596db7f47d79b9b691f9b8ec4fc0abc7c1e60595
Saqlain Haider <saqlainhaider77@gmail.com>
2025-06-05 11:46:01 +0500

    results desktop done

--- Ash-p/src/components/Home/Results/Results.tsx
+++ Ash-p/src/components/Home/Results/Results.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import { resultsContent } from './resultsContent';
 import './Results.scss';
+import spark from '../../../assets/home/resultSpark.svg'
 
 const Results: React.FC = () => {
     return (
         <section className="results-section">
+            <div className="spark-wrapper">
+                <img src={spark} alt="spark-graphic" />
+            </div>
             <h2 className="results-heading">{resultsContent.heading}</h2>
             <div className="results-cards">
                 {resultsContent.cards.map((card, index) => (
