4379e5b519e4615c45dd59813e4b9d629715000d
Saqlain Haider <saqlainhaider77@gmail.com>
2025-06-07 06:51:33 +0500

    testimonials

--- Ash-p/src/components/Home/Testimonial/Testimonial.tsx
+++ Ash-p/src/components/Home/Testimonial/Testimonial.tsx
@@ -54,7 +54,9 @@ const Testimonial: React.FC = () => {
                     onClick={prevSlide}
                     aria-label="Previous testimonial"
                 >
-                    ←
+                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M15 18L9 12L15 6" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+                    </svg>
                 </button>
 
                 <div ref={sliderRef} className={`${styles.carousel} keen-slider`}>
@@ -75,7 +77,9 @@ const Testimonial: React.FC = () => {
                     onClick={nextSlide}
                     aria-label="Next testimonial"
                 >
-                    →
+                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M9 6L15 12L9 18" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+                    </svg>
                 </button>
             </div>
         </section>
