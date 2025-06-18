df9ca4ece8fa27f06521ec18d799168232e2f536
Saqlain Haider <saqlainhaider77@gmail.com>
2025-06-19 02:37:47 +0500

    freediehero

--- Ash-p/src/components/freedie/FreedieHero/FreedieHero.tsx
+++ Ash-p/src/components/freedie/FreedieHero/FreedieHero.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import styles from './FreedieHero.module.scss';
+import flower from '../../../assets/freedi/freedieFlowers.svg'
 import { freedieHeroContent } from './content';
 
 const FreedieHero: React.FC = () => {
     return (
         <div className={styles.freedieHero}>
+             <div className={`${styles.flower} ${styles.small} ${styles.left}`}>
+                <img src={flower} alt="bg-flower" />
+            </div>
+            <div className={`${styles.flower} ${styles.small} ${styles.right}`}>
+                <img src={flower} alt="bg-flower" />
+            </div>
+            <div className={`${styles.flower} ${styles.small} ${styles.bottom}`}>
+                <img src={flower} alt="bg-flower" />
+            </div>
             <h2 className={styles.mainHeading}>
                 {freedieHeroContent.mainHeading.default}
                 <span className={styles.highlighted}>
@@ -21,6 +31,9 @@ const FreedieHero: React.FC = () => {
                 <p className={styles.cardText}>
                     {freedieHeroContent.cardText}
                 </p>
+            </div>
+            <div className={`${styles.flower} ${styles.medium}`}>
+                <img src={flower} alt="bg-flower" />
             </div>
         </div>
     );
