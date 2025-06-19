2522b4da8f96ad5d5e44235419858014772641dc
Saqlain Haider <saqlainhaider77@gmail.com>
2025-06-20 03:52:05 +0500

    Update FreedieSlider.tsx

--- Ash-p/src/components/freedie/freedieSlider/FreedieSlider.tsx
+++ Ash-p/src/components/freedie/freedieSlider/FreedieSlider.tsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import { useGSAP } from '@gsap/react'
+import gsap from 'gsap'
 import './FreedieSlider.scss'
+
 const FreedieSlider = () => {
+  // Create refs for the container elements
+  const containerRef = useRef(null);
+  const activeSlideRef = useRef(null);
+
+  useGSAP(() => {
+    // Get all slides and active slide images using refs and gsap.utils.toArray
+    const slides = gsap.utils.toArray(".slide", containerRef.current);
+    const activeSlideImages = gsap.utils.toArray("img", activeSlideRef.current);
+
+    const getInitialTranslateZ = (slide: HTMLElement): number => {
+        const style = window.getComputedStyle(slide);
+        const matrix = style.transform.match(/matrix3d\((.+)\)/);
+        if(matrix){
+            const values = matrix[1].split(", ");
+            const value = values[14];
+            return value ? parseFloat(value) : 0;
+        }
+        return 0;
+    }
+
+    const mapRange = (value: number, inMin: number, inMax: number, outMin: number, outMax: number): number => {
+        return ((value - inMin)*(outMax - outMin))/(inMax - inMin) + outMin;
+    }
+
+    slides.forEach((slide, index) =>{
+        const intialZ = getInitialTranslateZ(slide as HTMLElement);
+    })
+    // Your GSAP animations can go here
+    // The context will be maintained within this component
+  }, { scope: containerRef }); // Scope the animations to the entire container
+
   return (
-    <div className="slider-container">
-        <div className="active-slide">
+    <div className="slider-container" ref={containerRef}>
+        <div className="active-slide" ref={activeSlideRef}>
             <img src='./dan.jpg' alt="" />
             <img src='./dan.jpg' alt="" />
             <img src='./dan.jpg' alt="" />
