59237f42089f72adb9ca7969c86a0b20ac8ea76d
Saqlain Haider <saqlainhaider77@gmail.com>
2025-06-23 22:19:38 +0500

    Update ContactBar.tsx

--- Ash-p/src/components/contact/contactHero/contactBar/ContactBar.tsx
+++ Ash-p/src/components/contact/contactHero/contactBar/ContactBar.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import styles from "./contactBar.module.scss";
 import { contactBarContent } from "./batContent";
+import cphone from "../../../../assets/contact/cbPhone.svg"
+import mail from "../../../../assets/contact/EnvelopeSimple.svg"
+import copy from "../../../../assets/contact/copy.svg"
 
 const ContactBar: React.FC = () => {
   const { email, phone } = contactBarContent;
@@ -15,7 +18,7 @@ const ContactBar: React.FC = () => {
       {/* Email Section */}
       <div className={styles.emailSection}>
         {/* TODO: Add email icon here */}
-        <span className={styles.emailIcon}>{/* Email Icon */}</span>
+        <span className={styles.emailIcon}><img src={mail} alt="" /></span>
         <span>{email}</span>
       </div>
       {/* Copy Button */}
@@ -25,11 +28,11 @@ const ContactBar: React.FC = () => {
         aria-label="Copy email address"
       >
         {/* TODO: Add copy icon here */}
-        <span>{/* Copy Icon */}</span>
+        <span><img src={copy} alt="" /></span>
       </button>
       {/* Phone Section */}
       <div className={styles.phoneSection}>
-        <span className={styles.phoneIcon}>{/* Phone Icon */}</span>
+        <span className={styles.phoneIcon}><img src={cphone} alt="" /></span>
         <span>{phone}</span>
       </div>
     </div>
